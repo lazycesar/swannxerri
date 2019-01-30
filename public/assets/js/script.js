2d4cf56cd7318785ca820c3b68790dd9032e552d
lazycesar <cesarespi36@hotmail.com>
2019-01-30 18:46:05 +0100

    prefix css

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -23,3 +23,18 @@ ScrollReveal().reveal(".about__left__image", slideLeft);
 ScrollReveal().reveal(".slideLeft", slideLeft);
 ScrollReveal().reveal(".slideRight", slideRight);
 ScrollReveal().reveal(".card-feedback", { interval: 600 });
+
+// Get the container element
+var btnContainer = document.getElementById("HomeNav");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("header__menu__item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
