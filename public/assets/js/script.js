4b90e172759e59e4f47c01bcc579a4059a8da296
lazycesar <cesarespi36@hotmail.com>
2019-01-30 18:33:51 +0100

    update revealjs

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,4 +5,21 @@ $(document).ready(function() {
   });
 });
 
-ScrollReveal().reveal(".formation__left__image");
+var slideRight = {
+  distance: "100px",
+  origin: "right",
+  opacity: 0,
+  interval: 1000
+};
+var slideLeft = {
+  distance: "100px",
+  origin: "left",
+  opacity: 0,
+  interval: 1000
+};
+
+ScrollReveal().reveal(".formation__left__image", slideRight);
+ScrollReveal().reveal(".about__left__image", slideLeft);
+ScrollReveal().reveal(".slideLeft", slideLeft);
+ScrollReveal().reveal(".slideRight", slideRight);
+ScrollReveal().reveal(".card-feedback", { interval: 600 });
